b8d9b43b543581bc1a5bc5b6c8fd73c80ccb5987
Anug Saha <anugsaha@gmail.com>
2022-08-24 17:02:19 -0400

    incorporate spring backend

---
+++ flatiron-messaging-backend/src/main/java/com/example/flatironmessagingbackend/FlatironMessagingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.flatironmessagingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatironMessagingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatironMessagingBackendApplication.class, args);
+	}
+
+}
---
+++ flatiron-messaging-backend/src/main/java/com/example/flatironmessagingbackend/User.java
@@ -0,0 +1,12 @@
+package com.example.flatironmessagingbackend;
+
+public class User {
+
+    public String firstName;
+
+    public User(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+}
